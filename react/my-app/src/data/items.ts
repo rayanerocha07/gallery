390c6185e17513948bf3787fcfaaaa2b02dda994
Rayane Rocha <rayane.rocha07@gmail.com>
2021-11-30 11:30:26 -0300

    escape

---
+++ react/my-app/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  { date: new Date(2021, 10, 29), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2021, 10, 29), category: 'food', title: 'Burguer King', value: 28 },
+  { date: new Date(2021, 10, 30), category: 'rent', title: 'Aluguel', value: 2000 },
+  { date: new Date(2021, 11, 1), category: 'salary', title: 'Salário', value: 4500 }
+];
